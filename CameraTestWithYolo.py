dad2fb755e06161a676383743a9b4954f612d005
Siu Cheng <herrcheng@gmail.com>
2022-11-06 23:56:26 +0100

    refactor code :sheep:🤳

--- CameraTestWithYolo.py
+++ CameraTestWithYolo.py
@@ -1,15 +1,15 @@
 import cv2
 import numpy as np
 
-## load yolov3 tiny pretrained with coco
+# Load yolov3 tiny pretrained with coco
 yolo = cv2.dnn.readNet("./yolo/yolov3-tiny.weights", "./yolo/darknet-master/cfg/yolov3-tiny.cfg")
 
-## read class names from coco data
+# Read class names from coco data
 classes = []
 with open("./yolo/darknet-master/data/coco.names", 'r') as f:
     classes = f.read().splitlines()
 
-## read test image and get size
+# Read test image and get size
 vid = cv2.VideoCapture(0)
 W, H = 1920, 1080
 vid.set(cv2.CAP_PROP_FRAME_WIDTH, W)
@@ -20,39 +20,40 @@ fps = vid.get(cv2.CAP_PROP_FPS)
 print("fps: " + str(fps))
 
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
+font = cv2.FONT_HERSHEY_PLAIN
 
-while (True):
+while True:
+
+    # Press 'q' to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
     # Capture the video frame by frame
     ret, img = vid.read()
 
     height, width, _ = img.shape
 
-    # Press 'q' to quit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
     resized = cv2.resize(img, (320, 320))
 
-    ## preprocess image:
-    blob = cv2.dnn.blobFromImage(resized, 1 / 255, (320, 320), (0, 0, 0), swapRB=True, crop=False)
+    # Preprocess image:
+    blob = cv2.dnn.blobFromImage(resized, 1 / 255, (320, 320), (0, 0, 0), swapRB=False, crop=False)
 
-    ## use yolo to analyze image
+    # Use yolo to analyze image
     yolo.setInput(blob)
 
     output_layer_names = yolo.getUnconnectedOutLayersNames()
-    layeroutput = yolo.forward(output_layer_names)
+    layer_output = yolo.forward(output_layer_names)
 
     boxes = []
     confidences = []
     class_ids = []
 
-    for output in layeroutput:
+    for output in layer_output:
         for detection in output:
             score = detection[5:]
             class_id = np.argmax(score)
             confidence = score[class_id]
-            # get bounding box and class if confidence over .7
+            # Get bounding box and class if confidence over .7
             if confidence > 0.5:
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
@@ -67,8 +68,7 @@ while (True):
                 class_ids.append(class_id)
 
     indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-    font = cv2.FONT_HERSHEY_PLAIN
-    counter = 0
+    # counter = 0
     if len(indexes) > 0:
         for i in indexes.flatten():
             x, y, w, h = boxes[i]
@@ -77,7 +77,7 @@ while (True):
             # if label.lower() != "sheep":
             #     continue
 
-            counter += 1
+            # counter += 1
             config = str(round(confidences[i], 2))
             color = colors[i]
 
